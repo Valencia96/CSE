732023a615524cdcbabe64056ed4f4775c506399
Valencia96 <42385622+Valencia96@users.noreply.github.com>
2018-11-05 09:45:09 -0800

    11/5/2018
    Guess Game progress.

--- notes/Nicholas Oropeza - Guess Game.py
+++ notes/Nicholas Oropeza - Guess Game.py
@@ -1,9 +1,22 @@
 import random
 random_number = random.randint(1, 10)
-guess = int(input("Enter a random number between 1 and 10"))
 guesses = 5
-while guess != random_number and guesses < 0:
+
+
+def guess():
+    int(input("Enter a random number between 1 and 10"))
+
+
+def start():
+    guess()
+
+
+while guess != random_number and guesses > 0:
     if guess < random_number:
         print("Higher")
+        guesses -= 1
+        start()
     else:
         print("Lower")
+        guesses -= 1
+        start()
