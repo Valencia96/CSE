6f1317fc79d493d3ca3c12ab558a936bc065158f
Valencia96 <42385622+Valencia96@users.noreply.github.com>
2018-11-08 09:46:05 -0800

    11/8/2018
    Lucky 7s progress.

--- notes/Nicholas Oropeza - Lucky 7s.py
+++ notes/Nicholas Oropeza - Lucky 7s.py
@@ -3,10 +3,27 @@ money = int(15)
 playing = True
 d6_1 = random.randint(1, 6)
 d6_2 = random.randint(1, 6)
-bet = (int(input("Make a bet")))
+added_dice = int(d6_1 + d6_2)
+bet = (int(1))
 
-money - bet
+print()
+print("You bet %d Canadian Peso(s)" % bet)
+while money > 0 and playing:
+    print 1 (added_dice)
+    print()
+if added_dice == 7:
+    money -= 1
+    print("You have %d Canadian Peso(s)" % money)
+    print("You bet another Canadian Peso")
+    print()
+else:
+    money == bet + 4
+    print("You have %d Canadian Peso(s)" % money)
+    print()
+    print("You bet another Canadian Peso")
+    print()
+    money -= 1
 
-while d6_1+d6_2 != 7:
-    bet - 1
-
+while money == 0:
+    playing = False
+    print("You lost all of your money, loser.")
