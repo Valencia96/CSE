713b3792a5f120e37a7edabaf2d6a8bef73d3b78
Valencia96 <42385622+Valencia96@users.noreply.github.com>
2018-11-13 09:45:32 -0800

    11/13/2018
    Lucky 7s

--- notes/Nicholas Oropeza - Lucky 7s.py
+++ notes/Nicholas Oropeza - Lucky 7s.py
@@ -1,32 +1,38 @@
 import random
 money = int(15)
+max_money = (money > 15)
 playing = True
 d6_1 = random.randint(1, 6)
 d6_2 = random.randint(1, 6)
-roll = int(d6_1 + d6_2)
+roll = d6_1 + d6_2
 bet = (int(1))
+turn = int(0)
 
 print("You have %d Canadian Pesos" % money)
 print()
 print("You bet one Canadian Peso")
 while money > 0 and playing:
-    print(roll)
+    d6_1 = random.randint(1, 6)
+    d6_2 = random.randint(1, 6)
+    roll = d6_1 + d6_2
+    print("You got a %d " % roll)
     print()
+    turn += 1
     if roll == 7:
-        money == bet + 4
+        money = money + bet + 4
         print("You have %d Canadian Peso(s)" % money)
         print()
         print("You bet another Canadian Peso")
         print()
-        roll = int(d6_1 + d6_2)
     else:
         money -= 1
         print("You have %d Canadian Peso(s)" % money)
         print()
         print("You bet another Canadian Peso")
         print()
-        roll = int(d6_1 + d6_2)
 
-while money == 0:
-    playing = False
+while money == 0 and playing:
     print("You lost all of your money, loser.")
+    print("You lasted %d turns" % turn)
+    print("You had the most money you had was %d pesos" % max_money)
+    playing = False
