4e327a84ec99b2d09b12c8dc71d5d5ea7ab0ecbb
Valencia96 <42385622+Valencia96@users.noreply.github.com>
2018-11-15 09:45:34 -0800

    11/15/2018
    Added comments to "Lucky 7s" to explain my code if I ever come back to it.

--- notes/Nicholas Oropeza - Hangman.py
+++ notes/Nicholas Oropeza - Hangman.py
@@ -1,1 +1,7 @@
 import random
+playing = True
+guesses_left = 6
+letter = input("Enter a letter ")
+
+while guesses_left > 0 and playing:
+    placeholder
--- notes/Nicholas Oropeza - Lucky 7s.py
+++ notes/Nicholas Oropeza - Lucky 7s.py
@@ -1,29 +1,34 @@
 import random
-money = int(15)
-max_money = int(15)
-max_roll = int(0)
+"The is a gambling game in where you can't win and where the money isn't real, "
+"called 'Lucky 7s'"
+
+
+money = int(15)  # The amount of money you start with
+max_money = int(15)  # The most amount of money you've had
+max_roll = int(0)  # The roll on which you had the most money
 playing = True
-d6_1 = random.randint(1, 6)
-d6_2 = random.randint(1, 6)
-roll = d6_1 + d6_2
-bet = (int(1))
-turn = int(0)
+d6_1 = random.randint(1, 6)  # Die one
+d6_2 = random.randint(1, 6)  # Die two
+roll = (d6_1 + d6_2)  # The added dice number
+bet = (int(1))  # The amount of money you have bet
+turn = int(0)  # The turn number
 
 
-print("Turn: %d" % turn)
-print("You have %d Canadian Pesos" % money)
-print("You bet one Canadian Peso")
+print("Turn: %d" % turn)  # The current turn number
+print("You have %d Canadian Pesos" % money)  # The amount of money you currently have
+print("You bet one Canadian Peso")  # Note, the Canadian peso is not the real currency of Canada
 while money > 0 and playing:
-    d6_1 = random.randint(1, 6)
+    d6_1 = random.randint(1, 6)  # The dice 'roll' is stated again to re-roll the dice
     d6_2 = random.randint(1, 6)
-    roll = d6_1 + d6_2
+    roll  # This just adds the dice rolls each turn
     print()
     print("You got a %d " % roll)
     print()
-    turn += 1
+    turn += 1  # Adds one to the turn counter
     if max_money < money:
         max_money = money
         max_roll = roll
+    # Each time you get a 7 you win 5 pesos
     if roll == 7:
         money = money + bet + 4
         print("Turn: %d" % turn)
@@ -39,7 +44,7 @@ while money > 0 and playing:
 
 while money == 0 and playing:
     if max_roll == 0:
-        print("You shouldn't have come here.")
+        print("You shouldn't have come here.")  # As in you were really unlucky
     print()
     print("You lasted %d turns" % turn)
     print("You had the most money you had was %d pesos" % max_money)
