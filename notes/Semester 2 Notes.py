d0c111ed5bfbd0233c9e680eba048f017147cd63
Nicholas Oropeza Valencia 9819 <2jxt@fresnou.org>
2019-01-08 09:45:00 -0800

    1/8/2019
    Sem. 2 notes

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+# Super Mario Bros. 2 Best Game of the Year since 1988.
+cars = int(5)
+driving = True
+print("I have %d cars" % cars)
+print("I have " + str(cars) + " cars")
+age = input("How old are you?")
+print(age + "? You're so old you belong in a museum!")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+colors.append("cyan")
+print(colors)
+colors.pop(0)
+print(colors)
+print(colors[2])
+print(len(colors))
