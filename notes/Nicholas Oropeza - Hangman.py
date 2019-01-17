68a0020b3d128088cf9464d11c404f33d7a78b92
Nicholas Oropeza Valencia 9819 <2jxt@fresnou.org>
2019-01-17 09:45:19 -0800

    1/17/2019
    Hangman, added if statements.

--- notes/Nicholas Oropeza - Hangman.py
+++ notes/Nicholas Oropeza - Hangman.py
@@ -11,10 +11,8 @@ word_bank = ["katakana", "Japan", "Moscow", "block",
              "Nintendo Switch", "artificial", "intelligence",
              "How are you today?", "This is a sea urchin.",
              "Super Mario Bros. 2, GOTY Since 1988."]
-
-pesto = random.choice(word_bank)
+random_choice = random.choice(word_bank)
 letters_guessed = []
-hidden_word = len(pesto)
 guesses = 8
 playing = True
 
@@ -22,10 +20,16 @@ while guesses > 0 and playing:
     letter = input("Enter the letter or word")
     letters_guessed.append(letter)
     print(letters_guessed)
-
-    for i in range(len(pesto)):  # i goes through all indices
-        if pesto[0] == string.ascii_letters:  # if we find a letter
-            pesto.pop(i)  # remove the i-th index
-            pesto.insert(i, "_")  # Put a _ there instead
-            print(pesto)
-    guesses -= 1
+    
+    for i in range(len(random_choice)):  # i goes through all indices
+        if random_choice == list(string.ascii_letters):  # if we find a letter
+            random_choice(i)  # remove the i-th index
+            random_choice.insert(i, "_")  # Put a _ there instead
+   
+    if letter != random_choice:
+        print("Wrong, try again")
+        guesses -= 1
+    else:
+        print("Good guess")
+    if guesses == 0:
+        print(random_choice)
