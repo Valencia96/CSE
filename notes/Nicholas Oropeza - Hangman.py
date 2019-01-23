f6859152cd89e136d34b9e99918a6a25a3fd8c79
Nicholas Oropeza Valencia 9819 <2jxt@fresnou.org>
2019-01-23 09:44:47 -0800

    1/23/2019
    Code now looks for punctuation and ascii characters.

--- notes/Nicholas Oropeza - Hangman.py
+++ notes/Nicholas Oropeza - Hangman.py
@@ -9,34 +9,44 @@ word_bank = ["katakana", "Japan", "Moscow", "block",
              "No one expects the Spanish Inquisition.",
              "mouse", "turquoise", "concatenation", "jukebox",
              "Nintendo Switch", "artificial", "intelligence",
-             "How are you today?", "This is a sea urchin.",
-             "Super Mario Bros. 2, GOTY Since 1988."]
+             "How are you today?", "This is a sea urchin."]
+
 random_choice = random.choice(word_bank)
 letters_guessed = []
+blank = []
 guesses = 8
 playing = True
+
+print("Welcome to Hangman")
+
+for i in range(len(random_choice)):
+    if random_choice[i] in list(string.ascii_letters):
+        blank.append(random_choice)
+    elif random_choice[i] == "":
+        blank.append(" ")
+    else:
+        blank.append("_")
 
 while guesses > 0 and playing:
     letter = input("Enter the letter or word")
     letters_guessed.append(letter)
     print(letters_guessed)
-    
-    for i in range(len(random_choice)):  # i goes through all indices
-        if random_choice[i] in list(string.ascii_letters):  # if we find a letter
-            letters_guessed.append(random_choice)
-        elif random_choice[i]:
+    print()
 
-    print("".join(random_choice))
-
-    if letter != random_choice:
-        print("Wrong, try again")
-        guesses -= 1
-        print("You have %d guesses left" % guesses)
-        print()
+    for i in range(len(random_choice)):
+        if random_choice[i] in list(string.punctuation):
+            blank.append(random_choice)
+        else:
+            blank.append("_")
 
     if letter == random_choice:
-        print("You win!")
+        print("You guessed right, good job.")
         playing = False
 
+    if letter in random_choice:
+        print("Correct")
+    else:
+        guesses -= 1
+
     if guesses == 0:
-        print("The word was '%s'" % random_choice)
+        print("The word/phrase was '%s'" % random_choice)
