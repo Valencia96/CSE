06c910c9d6a29503ab3b34ef11200738d898f35f
Nicholas Oropeza Valencia 9819 <2jxt@fresnou.org>
2019-01-28 09:44:43 -0800

    1/28/2019

--- notes/Nicholas Oropeza - Hangman.py
+++ notes/Nicholas Oropeza - Hangman.py
@@ -8,22 +8,15 @@ word_bank = ["katakana", "Japan", "Moscow", "block",
              "How are you today?", "This is a sea urchin."]
 
 word = random.choice(word_bank)
-letters_guessed = []
+guessed = []
 blank = []
 guesses = 8
 playing = True
 
 print("Welcome to Hangman")
 print()
-
-while guesses > 0 and playing:
-    letter = input("Enter the letter or word")
-    letters_guessed.append(letter)
-    print(letters_guessed)
-    print(blank)
-    print("".join(blank))
-
-    for i in range(len(word)):
+print(word)
+for i in range(len(word)):
         if word[i] in list(string.ascii_letters):
             blank.append('_')
         elif word[i] == ' ':
@@ -33,17 +26,19 @@ while guesses > 0 and playing:
         elif word[i] == '.':
             blank.append('.')
 
+while guesses > 0 and playing:
+    letter = input("Enter the letter or word")
+    guessed.append(letter)
+    print(guessed)
+    print("".join(blank))
+
     for i in range(len(blank)):
         if letter in list(blank):
-            blank.append(letter)
+            blank.insert(i, letter)
+            print("".join(blank))
 
-    if letter in word:
-        blank.append(letter)
-        print("".join(blank))
-        print("Correct")
-        print("You have %d guesses left." % guesses)
-    elif letter in list(string.ascii_uppercase) and word:
-        blank.append(letter)
+    if letter.lower() in word:
+        blank.insert(i, letter)
         print("".join(blank))
         print("Correct")
         print("You have %d guesses left." % guesses)
@@ -52,9 +47,18 @@ while guesses > 0 and playing:
         print("Wrong")
         print("You have %d guesses left." % guesses)
 
+    if guessed == word:
+        print("You guessed right, good job.")
+        playing = False
+
     if letter == word:
         print("You guessed right, good job.")
         playing = False
 
     if guesses == 0:
         print("The word/phrase was '%s'" % word)
+#     elif letter in list(string.ascii_uppercase) and word:
+#         blank.insert(i, letter)
+#         print("".join(blank))
+#         print("Correct")
+#         print("You have %d guesses left." % guesses)
