d5596367f13d7a6d3c59d14260087faed1d006f0
Nicholas Oropeza Valencia 9819 <2jxt@fresnou.org>
2019-01-31 09:44:59 -0800

    1/31/2019

--- notes/Nicholas Oropeza - Dictionary World Map.py
+++ notes/Nicholas Oropeza - Dictionary World Map.py
@@ -1,7 +1,7 @@
 world_map = {
     'R19A': {
         'NAME': "Mr. Wiebe's Room",
-        'DESCRIPTION': "This is the classroom you are in right now." 
+        'DESCRIPTION': "This is the classroom you are in right now. " 
                        "There are two doors on the north wall",
         'PATHS': {
             'NORTH': "PARKING_LOT"
@@ -19,9 +19,20 @@ world_map = {
 # Controller
 playing = True
 current_node = world_map['R19A']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
+
 while playing:
     print(current_node['NAME'])
     print(current_node['DESCRIPTION'])
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
         playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way")
+
+    else:
+        print("Command Not Found")
