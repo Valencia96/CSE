39380ddbf241b9f3c5d802aa98a3e84e328c95d6
Nicholas Oropeza Valencia 9819 <2jxt@fresnou.org>
2019-02-06 09:44:01 -0800

    2/6/2019

---
+++ notes/Nicholas Oropeza - Dictionary World Map .py
@@ -0,0 +1,10 @@
+world_map = {
+    'A CLOSET': {
+        'NAME': "THE CLOSET",
+        'DESCRIPTION': "You're in the closet. There's a lock on the door." 
+                       "Check your pockets by pressing 'e'",
+        'PATHS': {
+              'WEST': "SHAGGY'S_ROOM"
+        }
+    },
+}
