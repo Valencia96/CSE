7cdd4ba65398273d1b726a304bb173f903ab7fa6
Nicholas Oropeza Valencia 9819 <2jxt@fresnou.org>
2019-03-05 09:44:47 -0800

    3/5/2019

--- notes/Items - Nicholas Oropeza.py
+++ notes/Items - Nicholas Oropeza.py
@@ -8,6 +8,14 @@ class Consumable(Item):
         super(Item, self).__init__(name)
 
 
+class Weapon(Item):
+    def __init__(self, name, damage_out, durability):
+        super(Weapon, self).__init__(name)
+        self.damage_out = damage_out
+        self.durability = durability
+        
+
+
 class Armor(Item):
     def __init__(self, name, damage_absorb, durability):
         super(Item, self).__init__(name)
@@ -21,7 +29,21 @@ class Food(Consumable):
         self.quantity = quantity
         self.health_recovered = health_rec
 
+
 class Steak(Food):
-    def __init__(self, name, quantity, consumed, health_rec):
+    def __init__(self, name, quantity, health_rec, consumed=False):
         super(Steak, self).__init__(name, quantity, health_rec)
-        self.consumed = False
+        self.consumed = consumed
+
+
+class LeatherHelmet(Armor):
+    def __init__(self, name, damage_absorb, durability, quantity):
+        super(LeatherHelmet, self).__init__(name)
+        self.quantity = quantity
+        self.damage_absorb = damage_absorb
+        self.durability = durability
+        
+        
+class WoodenSword(Weapon):
+    def __init__(self, name, ):
+        super(WoodenSword, self).__init__(name)
