9a779302063b5418b03e6ac62b8eb5dd1e9e8ea9
Nicholas Oropeza Valencia 9819 <2jxt@fresnou.org>
2019-03-06 09:45:01 -0800

    3/6/2019

--- notes/Items - Nicholas Oropeza.py
+++ notes/Items - Nicholas Oropeza.py
@@ -9,18 +9,19 @@ class Consumable(Item):
 
 
 class Weapon(Item):
-    def __init__(self, name, damage_out, durability):
+    def __init__(self, name, damage_out, durability, equipped=False):
         super(Weapon, self).__init__(name)
         self.damage_out = damage_out
         self.durability = durability
-        
+        self.equipped = equipped
 
 
 class Armor(Item):
-    def __init__(self, name, damage_absorb, durability):
+    def __init__(self, name, damage_absorb, durability, equipped=False):
         super(Item, self).__init__(name)
         self.damage_absorb = damage_absorb
         self.durability = durability
+        self.equipped = equipped
 
 
 class Food(Consumable):
@@ -34,16 +35,24 @@ class Steak(Food):
     def __init__(self, name, quantity, health_rec, consumed=False):
         super(Steak, self).__init__(name, quantity, health_rec)
         self.consumed = consumed
+        self.health_rec = 10
 
 
 class LeatherHelmet(Armor):
     def __init__(self, name, damage_absorb, durability, quantity):
         super(LeatherHelmet, self).__init__(name)
         self.quantity = quantity
-        self.damage_absorb = damage_absorb
-        self.durability = durability
+        self.damage_absorb = 1
+        self.durability = 90
         
         
 class WoodenSword(Weapon):
-    def __init__(self, name, ):
+    def __init__(self, name):
         super(WoodenSword, self).__init__(name)
+        self.damage_out = 2
+        self.durability = 60
+
+
+class LeatherChestplate(Armor):
+    def __init__(self, name):
+        super(LeatherChestplate, self).__init__(name)
