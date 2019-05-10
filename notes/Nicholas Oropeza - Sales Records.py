89fa5aba2dc5906ae095d8b5655484336f0fc076
Valencia96 <42385622+Valencia96@users.noreply.github.com>
2019-05-10 10:39:02 -0700

    5/10/2019

--- notes/Nicholas Oropeza - Sales Records.py
+++ notes/Nicholas Oropeza - Sales Records.py
@@ -1,20 +1,19 @@
 import csv
 
-cloth_profit = 0
-fruit_profit = 0
-meat_profit = 0
-beverage_profit = 0
-office_profit = 0
-cosmetic_profit = 0
-snack_profit = 0
-care_profit = 0
-house_profit = 0
-vegetable_profit = 0
-baby_profit = 0
-cereal_profit = 0
-
 with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
+    cloth_profit = 0
+    fruit_profit = 0
+    meat_profit = 0
+    beverage_profit = 0
+    office_profit = 0
+    cosmetic_profit = 0
+    snack_profit = 0
+    care_profit = 0
+    house_profit = 0
+    vegetable_profit = 0
+    baby_profit = 0
+    cereal_profit = 0
     print("Processing...")
     for row in reader:
         if row[2] == 'Clothes':
@@ -65,28 +64,28 @@ with open("Sales Records.csv", 'r') as old_csv:
             profits = float(row[13])
             for i in row[13]:
                 cereal_profit += profits
+
+print("The total amount of profits for clothes is: %d" % round(cloth_profit, 2))
+print()
+print("The total amount of profits for fruits is: %d" % round(fruit_profit, 2))
+print()
+print("The total amount of profits for meat is: %d" % round(meat_profit, 2))
+print()
+print("The total amount of profits for beverages is: %d" % round(beverage_profit, 2))
+print()
+print("The total amount of profits for office supplies is: %d" % round(office_profit, 2))
+print()
+print("The total amount of profits for cosmetics is: %d" % round(cosmetic_profit, 2))
+print()
+print("The total amount of profits for snacks is: %d" % round(snack_profit, 2))
+print()
+print("The total amount of profits for personal care is: %d" % round(care_profit, 2))
+print()
+print("The total amount of profits for household items is: %d" % round(house_profit, 2))
+print()
+print("The total amount of profits for vegetables is: %d" % round(vegetable_profit, 2))
+print()
+print("The total amount of profits for baby food is: %d" % round(baby_profit, 2))
+print()
+print("The total amount of profits for cereal is: %d" % round(cereal_profit, 2))
 print("Done")
-print("The total amount of profits for clothes is: %d" % cloth_profit)
-print()
-print("The total amount of profits for fruits is: %d" % fruit_profit)
-print()
-print("The total amount of profits for meat is: %d" % meat_profit)
-print()
-print("The total amount of profits for beverages is: %d" % beverage_profit)
-print()
-print("The total amount of profits for office supplies is: %d" % office_profit)
-print()
-print("The total amount of profits for cosmetics is: %d" % cosmetic_profit)
-print()
-print("The total amount of profits for snacks is: %d" % snack_profit)
-print()
-print("The total amount of profits for personal care is: %d" % care_profit)
-print()
-print("The total amount of profits for household items is: %d" % house_profit)
-print()
-print("The total amount of profits for vegetables is: %d" % vegetable_profit)
-print()
-print("The total amount of profits for baby food is: %d" % baby_profit)
-print()
-print("The total amount of profits for cereal is: %d" % cereal_profit)
-print()
